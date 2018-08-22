2637258759e5a3816061dd22330a6dbec6398d63
P.Tihomirov <pavel.tihomirov@ctco.lv>
2018-08-22 14:54:22 +0300

    Fixed: List of Topics to main page & got it Working

--- src/main/java/lv/ctco/javaschool/eventorganaizer/control/EventStore.java
+++ src/main/java/lv/ctco/javaschool/eventorganaizer/control/EventStore.java
@@ -1,16 +1,14 @@
 package lv.ctco.javaschool.eventorganaizer.control;
 
 import lv.ctco.javaschool.auth.control.UserStore;
-
 import lv.ctco.javaschool.eventorganaizer.entity.Event;
-
-
 import lv.ctco.javaschool.eventorganaizer.entity.EventStatus;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -23,10 +21,12 @@ public class EventStore {
     private UserStore userStore;
 
     public List<Event> getAllEvents() {
-        return em.createQuery("select e from Event e" +
+        List<Event> list = new ArrayList<>();
+        list = em.createQuery("select e from Event e" +
                 " where e.status = :status", Event.class)
                 .setParameter("status", EventStatus.OPEN)
                 .getResultList();
+        return list;
     }
 
 }
---
+++ src/test/java/lv/ctco/javaschool/eventorganaizer/boundary/EventOrganizationApiTest.java
@@ -0,0 +1,11 @@
+package lv.ctco.javaschool.eventorganaizer.boundary;
+
+import org.junit.jupiter.api.Test;
+
+class EventOrganizationApiTest {
+    @Test
+    void getAllOpenEvents() {
+//        get("/event").then().body(TopicListDto);
+    }
+
+}
