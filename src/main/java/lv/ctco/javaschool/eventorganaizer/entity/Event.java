cebd9fc37a5e0022b86baed80bbd9ce96dccc8ac
Dmitry Kiselyov <dmitry.kiselyov@ctco.lv>
2018-08-29 15:19:34 +0300

    #39 Add Page: add "agenda" textarea in new Master (fix 1)

--- src/main/java/lv/ctco/javaschool/eventorganaizer/entity/Event.java
+++ src/main/java/lv/ctco/javaschool/eventorganaizer/entity/Event.java
@@ -22,12 +22,15 @@ public class Event {
 
     private String description;
 
+    private String agenda;
+
     private EventStatus status;
 
-    public Event(String name, User author, String description, String date) {
+    public Event(String name, User author, String description, String agenda, String date) {
         this.name = name;
         this.author = author;
         this.description = description;
+        this.agenda = agenda;
         this.date = date;
         this.status = EventStatus.OPEN;
 
@@ -83,4 +86,8 @@ public class Event {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public String getAgenda() { return agenda; }
+
+    public void setAgenda(String agenda) { this.agenda = agenda; }
 }
