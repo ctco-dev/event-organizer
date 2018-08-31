ed90eba968e48e8af6bc9948fdb68af40282ff63
MargoSank <smsankova@gmail.com>
2018-08-31 10:39:05 +0300

    add getAnswerByID()

--- src/main/java/lv/ctco/javaschool/eventorganaizer/control/AnswersStore.java
+++ src/main/java/lv/ctco/javaschool/eventorganaizer/control/AnswersStore.java
@@ -1,13 +1,13 @@
 package lv.ctco.javaschool.eventorganaizer.control;
 
 import lv.ctco.javaschool.eventorganaizer.entity.Answer;
-import lv.ctco.javaschool.eventorganaizer.entity.Poll;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 
 @Stateless
 public class AnswersStore {
@@ -22,6 +22,14 @@ public class AnswersStore {
                 " where a.pollID=:id", Answer.class)
                 .setParameter("id", id)
                 .getResultList();
+    }
+
+    public Optional<Answer> getAnswerByID(Long id){
+        return em.createQuery("select  a from Answer a" +
+                " where a.id=:id", Answer.class)
+                .setParameter("id", id)
+                .getResultStream()
+                .findFirst();
     }
 
 }
